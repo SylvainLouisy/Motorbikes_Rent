81ca4f4a5a907a2531bd47f2596c619a3076189d
TheAliErrachid <errachid.ali.perso@gmail.com>
2023-08-21 15:47:18 +0200

    motorbikes controller set up except destroy

--- app/controllers/motorbikes_controller.rb
+++ app/controllers/motorbikes_controller.rb
@@ -1,2 +1,53 @@
 class MotorbikesController < ApplicationController
+before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @motorbikes = Motorbike.all
+  end
+
+  def new
+    @motorbike = Motorbike.new
+  end
+
+  def create
+    @motorbike = Motorbike.new(motorbike_params)
+    if @motorbike.save
+      redirect_to @motorbike
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @motorbike = Motorbike.find(params[:id])
+  end
+
+  def edit
+    @motorbike = Motorbike.find(params[:id])
+  end
+
+  def update
+    @motorbike = Motorbike.find(params[:id])
+    if @motorbike.update(motorbike_params)
+      redirect_to @motorbike
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # def destroy
+  #   @motorbike = Motorbike.find(params[:id])
+  #   @motorbike.destroy
+  #   redirect_to motorbikes_path, status: :see_other
+  # end
+
+  private
+
+  def set_motorbike
+    @motorbike = Motorbike.find(params[:id])
+  end
+
+  def motorbike_params
+    params.require(:motorbike).permit(:name, :brand, :color, :year, :price, :user_id)
+  end
 end
