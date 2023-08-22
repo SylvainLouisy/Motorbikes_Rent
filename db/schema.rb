80b05cc8ac843d7454e189da9e9344c9152c9272
Sylvain Louis Louisy <sylvain.louisy@hotmail.fr>
2023-08-22 10:28:45 +0200

    change rating type to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_141620) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_082010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_141620) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "content"
-    t.string "rating"
+    t.integer "rating"
     t.bigint "motorbike_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
