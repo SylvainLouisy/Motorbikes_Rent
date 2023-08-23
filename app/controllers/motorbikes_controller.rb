95b181d995f6c1775b4f8be5236e3d563e1ccd69
Guillaume Masson <yommy91300@gmail.com>
2023-08-23 14:33:43 +0200

    Added link_to to create a rent if already signed up

--- app/controllers/motorbikes_controller.rb
+++ app/controllers/motorbikes_controller.rb
@@ -1,5 +1,5 @@
 class MotorbikesController < ApplicationController
-before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     if params[:query].present?
