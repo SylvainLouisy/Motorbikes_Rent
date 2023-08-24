15cc94c10109bd3b8cd519753ca66df41760cf80
Guillaume Masson <yommy91300@gmail.com>
2023-08-24 12:43:42 +0200

    Added columns to motorbikes show

--- db/schema.rb
+++ db/schema.rb
@@ -53,14 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_132326) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
-  create_table "monuments", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.date "opening_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "motorbikes", force: :cascade do |t|
     t.string "name"
     t.string "brand"
