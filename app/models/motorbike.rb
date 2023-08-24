9267ad27eb515637c76e1bbe6de343a0d896abcf
Guillaume Masson <yommy91300@gmail.com>
2023-08-24 15:43:10 +0200

    Adding search bar

--- app/models/motorbike.rb
+++ app/models/motorbike.rb
@@ -6,10 +6,7 @@ class Motorbike < ApplicationRecord
   validates :name, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
   include PgSearch::Model
-  pg_search_scope :search_by_name_and_brand,
-    against: [ :name, :brand ],
-    using: {
-    tsearch: { prefix: true } # <-- now `superman batm` will return something!
-  }
+  multisearchable against: [:name, :brand]
 end
