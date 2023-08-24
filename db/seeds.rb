2f8d340af867e302297b3549649c9515c9a2385c
Guillaume Masson <yommy91300@gmail.com>
2023-08-24 17:24:10 +0200

    Added description to schema and improves index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,16 +22,16 @@ user4 = { email: "paul@gmail.com", password: "123456", name: "Paul", age: "21" }
 end
 
 puts "Creating motorbikes"
-motorbike1 = Motorbike.create(name: "YZF-R1", brand: "Yamaha", color: "Black", year: "2023", price: "50", address: "92 Bd de Clichy, 75018 Paris")
-motorbike2 = Motorbike.create(name: "Scoopy", brand: "Scoopy", color: "Red", year: "2018", price: "20", address: "1 Bd Montmartre, 75002 Paris")
-motorbike3 = Motorbike.create(name: "PCX", brand: "PCX", color: "Blue", year: "1993", price: "30", address: "7 Bd Montmartre, 75002 Paris")
-motorbike4 = Motorbike.create(name: "Ninja", brand: "Kawasaki", color: "White", year: "1994", price: "60", address: "34 Quai d'Austerlitz, 75013 Paris")
-motorbike5 = Motorbike.create(name: "Click 125", brand: "Click", color: "Yellow", year: "1995", price: "25", address: "32 Quai d'Austerlitz, 75013 Paris")
-motorbike6 = Motorbike.create(name: "ADV 350", brand: "Honda", color: "Black", year: "2016", price: "40", address: "4 Quai d'Austerlitz, 75013 Paris")
-motorbike7 = Motorbike.create(name: "Piaggio 1", brand: "Piaggio", color: "White", year: "2018", price: "30", address: "92 Bd de Clichy, 75018 Paris")
-motorbike8 = Motorbike.create(name: "Burgman", brand: "Suzuki", color: "White", year: "2021", price: "25", address: "2 Bd de Clichy, 75018 Paris")
-motorbike9 = Motorbike.create(name: "Primavera", brand: "Vespa", color: "Blue", year: "1995", price: "20", address: "92 Bd de Clichy, 75018 Paris")
-motorbike10 = Motorbike.create(name: "Savy", brand: "BMW", color: "Black", year: "2022", price: "25", address: "8 Rue Saint-Denis, 75001 Paris")
+motorbike1 = Motorbike.create(name: "YZF-R1", brand: "Yamaha", color: "Black", year: "2023", price: "50", address: "92 Bd de Clichy, 75018 Paris", description: "If you're tired to live book this demon!")
+motorbike2 = Motorbike.create(name: "Scoopy", brand: "Scoopy", color: "Red", year: "2018", price: "20", address: "1 Bd Montmartre, 75002 Paris",  description: "A confy way to move around in the city")
+motorbike3 = Motorbike.create(name: "PCX", brand: "PCX", color: "Blue", year: "2017", price: "30", address: "7 Bd Montmartre, 75002 Paris",  description: "A way to remember your days in Thailand")
+motorbike4 = Motorbike.create(name: "Ninja", brand: "Kawasaki", color: "White", year: "2020", price: "60", address: "34 Quai d'Austerlitz, 75013 Paris",  description: "Simply a beast, lazy drivers go away")
+motorbike5 = Motorbike.create(name: "Click 125", brand: "Click", color: "Yellow", year: "2022", price: "25", address: "32 Quai d'Austerlitz, 75013 Paris",  description: "A basic style but still efficient")
+motorbike6 = Motorbike.create(name: "ADV 350", brand: "Honda", color: "Black", year: "2016", price: "40", address: "4 Quai d'Austerlitz, 75013 Paris",  description: "A new generation, just give it a try")
+motorbike7 = Motorbike.create(name: "Piaggio", brand: "Piaggio", color: "White", year: "2018", price: "30", address: "92 Bd de Clichy, 75018 Paris",  description: "Italians do know how to make motorbikes!")
+motorbike8 = Motorbike.create(name: "Burgman", brand: "Suzuki", color: "White", year: "2021", price: "25", address: "2 Bd de Clichy, 75018 Paris",  description: "I know I don't look very pretty but please pick me")
+motorbike9 = Motorbike.create(name: "Primavera", brand: "Vespa", color: "Blue", year: "2015", price: "20", address: "92 Bd de Clichy, 75018 Paris",  description: "Old but classy, any ancester in the place ?")
+motorbike10 = Motorbike.create(name: "Savy", brand: "BMW", color: "Black", year: "2022", price: "50", address: "8 Rue Saint-Denis, 75001 Paris",  description: "From Germany, say less, peoples know!")
 
 file1 = URI.open("https://cdp.azureedge.net/products/USA/YA/2023/MC/SUPERSPORT/YZF-R1/50/PERFORMANCE_BLACK/2000000024.jpg")
 file2 = URI.open("https://imgcdn.zigwheels.co.th/large/gallery/color/90/1683/honda-scoopy-i-2018-color-580076.jpg")
