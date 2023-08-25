29f47b4c06c543e79a7192fd2ffa446d4539b5b9
TheAliErrachid <errachid.ali.perso@gmail.com>
2023-08-25 10:36:10 +0200

    adress added in new

--- app/models/motorbike.rb
+++ app/models/motorbike.rb
@@ -3,7 +3,7 @@ class Motorbike < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_one_attached :photo
-  validates :name, presence: true
+  validates :name, :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
